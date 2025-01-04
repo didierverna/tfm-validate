418ac5afbd4ad1961aa73ef5b93d188b9fc36920
Didier Verna <didier@didierverna.net>
2025-01-04 16:54:59 +0100

    Bump version number to 1.2.
    2025-01-04  Didier Verna  <didier@didierverna.net>
    
    	* setup/src/version.lisp: Update header copyright.
    	(*copyright-years*): Add 2025.
    	(*release-minor-level*): Set to 2.
    	(*release-name*): Set to "Early Gothic".
    	* make/version.make (LONG_VERSION): Update accordingly.

--- setup/src/version.lisp
+++ setup/src/version.lisp
@@ -1,6 +1,6 @@
 ;;; version.lisp --- TFM Validate version management
 
-;; Copyright (C) 2024 Didier Verna
+;; Copyright (C) 2024, 2025 Didier Verna
 
 ;; Author: Didier Verna <didier@didierverna.net>
 
@@ -31,7 +31,7 @@
 
 
 ;; Copyright years
-(defvar *copyright-years* "2024"
+(defvar *copyright-years* "2024, 2025"
   "A string denoting the copyright years for the whole project.")
 
 
@@ -41,7 +41,7 @@
 (defparameter *release-major-level* 1
   "The major level of this release.")
 
-(defparameter *release-minor-level* 1
+(defparameter *release-minor-level* 2
   "The minor level of this release.")
 
 (defparameter *release-status* :patchlevel
@@ -50,7 +50,7 @@
 (defparameter *release-status-level* 0
   "The status level of this release.")
 
-(defparameter *release-name* "QT Cloistered Monk"
+(defparameter *release-name* "Early Gothic"
   "The name of this release.
 The general naming theme for TFM Validate is \"Blackletter Fonts\",
 from the LaTeX Font Catalogue.
